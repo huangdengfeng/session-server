4204f6fe6442eeec4f36ded0952d62632b195ec1
dfenghuang <dfenghuang@tencent.com>
2024-07-20 17:11:05 +0800

    feat: gen java

--- entity/pb/gen.sh
+++ entity/pb/gen.sh
@@ -1,1 +1,2 @@
 protoc --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative --validate_out="lang=go,paths=source_relative:." *.proto
+protoc --plugin=protoc-gen-grpc-java --grpc-java_out=. --java_out=. --validate_out="lang=java:."  *.proto
